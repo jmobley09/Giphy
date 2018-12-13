908f3a060fdff63e07458799f781971fa72f4c9e
jmobley <joshua.mobley.jm@gmail.com>
2018-12-13 15:23:20 -0700

    buttons now working and pulling correct images from giphy

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -5,71 +5,47 @@ var Searches = ['hamster', 'parrot', 'fish'];
 
 // creates buttons and add to page
 function buttons() {
-    for (var i = 0; i < Searches.length; i++) {
-        var btnTag = $('<button>');
+  for (var i = 0; i < Searches.length; i++) {
+    var btnTag = $('<button>');
 
-        btnTag.attr('still-source', 'null');
-        btnTag.attr('animated-source', 'null');
-        btnTag.attr('name', Searches[i]);
-
-        btnTag.append(Searches[i]);
-        $('#game').append(btnTag);
-    }
+    btnTag.attr('name', Searches[i]);
+    btnTag.append(Searches[i]);
+    $('#game-buttons').append(btnTag);
+  }
 }
+buttons();
 
-// Loads the buttons to the page on screen load
-window.onload = function () {
-    buttons();
-}
+$("button").on("click", function () {
+  var SearchTerm = $(this).attr("name");
+  var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
+    SearchTerm + "&api_key=qXR34Xj4g5cgE8V3Bza9Q07qHGQUybQR&limit=10";
 
-$("button").on("click", function() {
-    var SearchTerm = $(this).attr("name");
-    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
-    SearchTerm + "&api_key=dc6zaTOxFJmzC&limit=10";
+  $.ajax({
+    url: queryURL,
+    method: "GET"
+  }).then(function (response) {
 
-    $.ajax({
-      url: queryURL,
-      method: "GET"
-    }).then(function(response) {
-      // Step 1: Run this file, click a button, and see what the response object looks like in the browser's console.
-      // Open up the data key, then open up the 0th, element. Study the keys and how the JSON is structured.
+    var results = response.data;
+    console.log(results);
+    // for loop that creates div and adds information recived from giphy
+    for (var i = 0; i < results.length; i++) {
 
-      console.log(response);
+      var searchDiv = $('<div>');
+      var p = $('<p>');
+      p.text('Rating: ' + results[i].rating);
 
-      // Step 2: since the image information is inside of the data key,
-      // make a variable named results and set it equal to response.data
+      // changes image settings of button that is clicked
+      var searchImage = $('<img>');
+      searchImage.attr('src', results[i].images.original.url);
+      searchDiv.append(p);
+      searchDiv.append(searchImage);
+      searchDiv.addClass('gif');
+      $('#gifs-appear-here').prepend(searchDiv);
       
-      // =============== put step 2 in between these dashes ==================
-      var results = response.data;
-      // ========================
-      console.log(results);
-      for (var i = 0; i < results.length; i++) {
+    }
 
-      // Step 3: uncomment the for loop above and the closing curly bracket below.
-      // Make a div with jQuery and store it in a variable named animalDiv.
-      // Make a paragraph tag with jQuery and store it in a variable named p.
-      // Set the inner text of the paragraph to the rating of the image in results[i].
-      // Make an image tag with jQuery and store it in a variable named animalImage.
-      // Set the image's src to results[i]'s fixed_height.url.
-      // Append the p variable to the animalDiv variable.
-      // Append the animalImage variable to the animalDiv variable.
-      // Prepend the animalDiv variable to the element with an id of gifs-appear-here.
 
-      // ============= put step 3 in between these dashes ======================
-        var animalDiv = $('<div>');
-        var p = $('<p>');
 
-        p.text('Rating: ' + results[i].rating);
-        
-        var animalImage = $('<img>');
-
-        animalImage.attr('src', results[i].images.fixed_height.url);
-
-        animalDiv.append(p);
-        animalDiv.append(animalImage);
-
-        $('#gifs-appear-here').prepend(animalDiv);
-      }
-
-    });
   });
+
+});
